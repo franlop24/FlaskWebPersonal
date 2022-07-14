a0edc9fa1657455b4741790c675e0c7da2272bc8
Francisco Lopez <franlopbri@gmail.com>
2022-07-14 15:03:40 -0500

    implementando for de peojects

--- web_personal/app.py
+++ web_personal/app.py
@@ -18,7 +18,33 @@ def contact():
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('public/portfolio.html')
+    projects = [
+        {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/home-bg.jpg',
+            'url': 'https://www.google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/about-bg.jpg',
+            'url': 'https://www.xataka.com'
+        },
+        {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/home-bg.jpg',
+            'url': 'https://www.google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/about-bg.jpg',
+            'url': 'https://www.xataka.com'
+        },
+    ]
+    return render_template('public/portfolio.html', projects=projects)
 
 ############ Rutas ############
 @app.route('/auth/login')
