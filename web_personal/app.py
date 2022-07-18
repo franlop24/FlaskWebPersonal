99fa159589857a9ec8286d5d863e05c98522f7ff
Francisco Lopez <franlopbri@gmail.com>
2022-07-18 18:46:53 -0500

    error handler para error 404

--- web_personal/app.py
+++ web_personal/app.py
@@ -64,5 +64,9 @@ def welcome():
 
     return render_template('admin/index.html', user_access=access)
 
+@app.errorhandler(404)
+def page_error_not_found(e):
+    return render_template('error/404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
