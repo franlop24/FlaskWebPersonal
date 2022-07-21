7077a2c75bb74df7cd20d32f443b6840240ecba4
Francisco Lopez <franlopbri@gmail.com>
2022-07-21 15:23:05 -0500

    Blueprint Error Pages con errorhandler

--- web_personal/app.py
+++ web_personal/app.py
@@ -1,23 +1,20 @@
 ############## Imports Flask & Python ##########
-from crypt import methods
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask
 
 ############## Imports Apps Blueprints ##########
 from home.views import home_blueprint
 from auth.views import auth_blueprint
+from error_pages.handlers import error_pages
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-
-
-@app.errorhandler(404)
-def page_error_not_found(e):
-    return render_template('error/404.html'), 404
+app.config['ENV'] = 'development'
 
 
 ################# Registro de Apps ##################
 app.register_blueprint(home_blueprint)
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
+app.register_blueprint(error_pages)
 
 if __name__ == '__main__':
     app.run(debug=True)
