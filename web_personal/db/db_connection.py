01eac37cda9687999c23cb9a335b19513e607402
Francisco Lopez <franlopbri@gmail.com>
2022-07-21 17:43:22 -0500

    Conectar a Base de Datos para Register

--- web_personal/db/db_connection.py
+++ web_personal/db/db_connection.py
@@ -4,6 +4,6 @@ def get_connection():
     connection = pymysql.connect(host='localhost',
                                  user='root',
                                  password='example',
-                                 db='platzi_blog',
+                                 db='web_personal',
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
