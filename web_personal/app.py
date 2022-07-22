9e82c274872185c7fc431175b58287efadb14e9b
Francisco Lopez <franlopbri@gmail.com>
2022-07-22 12:36:19 -0500

    Views de Projects

--- web_personal/app.py
+++ web_personal/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from home.views import home_blueprint
 from auth.views import auth_blueprint
 from error_pages.handlers import error_pages
+from projects.views import project_blueprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -14,6 +15,7 @@ app.config['ENV'] = 'development'
 app.register_blueprint(home_blueprint)
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(error_pages)
+app.register_blueprint(project_blueprint, url_prefix='/project')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ web_personal/projects/views.py
@@ -0,0 +1,63 @@
+################# Imorts de Flask & Python #################
+from flask import render_template, request, Blueprint
+
+project_blueprint = Blueprint('project', __name__)
+
+#### localhost:5000/project/list ####
+#### localhost:5000/project/34 ####
+#### localhost:5000/project/new ####
+#### localhost:5000/project/34/edit ####
+#### localhost:5000/project/34/delete ####
+
+################### TODO: List Projects #################
+@project_blueprint.route('/list')
+def list():
+    projects = [
+        {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/home-bg.jpg',
+            'url': 'https://www.google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/about-bg.jpg',
+            'url': 'https://www.xataka.com'
+        },
+        {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/home-bg.jpg',
+            'url': 'https://www.google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+            'image':'img/about-bg.jpg',
+            'url': 'https://www.xataka.com'
+        },
+    ]
+    return render_template('project/list.html', projects=projects)
+
+################### TODO: Show Project #################
+@project_blueprint.route('/<project_id>')
+def show(project_id):
+    return render_template('project/show.html', project_id=project_id)
+
+################### TODO: Create New Project #################
+@project_blueprint.route('/new')
+def new():
+    return render_template('project/new.html')
+
+################### TODO: Update a Project #################
+@project_blueprint.route('/<project_id>/edit')
+def edit(project_id):
+    return "Edit Page"
+
+################### TODO: Delete a Project #################
+@project_blueprint.route('/<project_id>/delete')
+def delete(project_id):
+    return "Delete Page"
+
+
