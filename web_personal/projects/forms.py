ba78eb85d7fda31f0fa1ae5c4b2a77e45f3a6812
Francisco Lopez <franlopbri@gmail.com>
2022-07-23 13:48:05 -0500

    Implementando validaciones en Form New project

--- web_personal/projects/forms.py
+++ web_personal/projects/forms.py
@@ -1,11 +1,17 @@
 ################## Imports Flask & Python Modules ##################
 from flask_wtf import FlaskForm
 from wtforms import (StringField, SubmitField, TextAreaField, FileField, URLField)
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, URL, Length
+from flask_wtf.file import FileAllowed
 
-class NewProjectForm(FlaskForm):
-    name = StringField('Nombre', validators=[DataRequired()])
+class FormProjectBase(FlaskForm):
+    name = StringField('Nombre', validators=[DataRequired(), Length(min=10, max=50)])
     description = TextAreaField('Descripción', validators=[DataRequired()])
-    url = URLField('URL', validators=[DataRequired()])
-    image = FileField('Imagen', validators=[DataRequired()])
+    url = URLField('URL', validators=[DataRequired(), URL()])
+    image = FileField('Imagen', validators=[DataRequired(), FileAllowed(['jpg', 'png'])])
+
+class NewProjectForm(FormProjectBase):    
     submit = SubmitField('Crear')
+
+class UpdateProjectForm(FormProjectBase):
+    submit = SubmitField('Actualizar')
